6a8cc8d2cc70eb52a85166a907363156fdeacb69
Anne Celestino <hubwub@tagspace.me>
2016-05-25 15:14:51 -0500

    added grsh alias for git; updated tmux.conf

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,6 +17,7 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias grsh='git reset --soft HEAD'
 alias grhh='git reset --hard HEAD'
 alias gsp="git log --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --since '1 week ago' --author 'Anne' --all"
 
