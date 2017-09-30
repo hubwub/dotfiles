51475a3c5916af8777e69483de72858d1d9c1c9a
Anne Celestino <hubwub@tagspace.me>
2017-09-29 20:46:56 -0500

    added brew and brew-cask installs

---
+++ install/macos/brew-cask.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# install cask and applications needed
+
+# install cask
+brew tap caskroom/cask
+brew tap caskroom/fonts
+brew tap caskroom/versions
+
+# packages to install
+apps=(
+  android-file-transfer
+  firefox
+  firefoxnightly
+  google-backup-and-sync
+  google-chrome
+  google-chrome-canary
+  gpgtools
+  iina
+  iterm2-nightly
+  keepassx
+  kid3
+  omnidisksweeper
+  osxfuse
+  postgres
+  simple-comic
+  spectacle
+  steam
+  transmission
+  ubersicht
+  xquartz
+)
+
+fonts=(
+  font-dejavu-sans
+  font-droid-sans
+  font-droid-sans-mono
+  font-inconsolata
+  font-inconsolata-dz
+  font-lato
+  font-meslo-lg
+  font-noto-sans
+  font-noto-serif
+  font-open-sans
+  font-roboto
+  font-roboto-mono
+)
+
+# install casks
+brew cask install "${apps[@]}"
+brew cask install "${fonts[@]}"
+
+# cleanup casks
+brew cask cleanup
---
+++ install/macos/brew.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# install homebrew
+
+# prompt for administrator password
+
+sudo -v
+
+# check if homebrew is installed/missing
+
+if test ! "$(which brew)"
+then
+  echo "installing homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew tap homebrew/bundle
+brew tap homebrew/core
+brew tap homebrew/services
+brew tap crisidev/homebrew-chunkwm
+
+# upgrade any existing/installed formulae
+brew upgrade
+
+apps=(
+  bash
+  bash-completion
+  binutils
+  chunkwm
+  coreutils
+  findutils
+  git
+  gnu-sed
+  grc
+  hr
+  httpie
+  hub
+  koekeishiya/formulae/skhd
+  mercurial
+  neofetch
+  pandoc
+  reattach-to-user-namespace
+  ripgrep
+  ssh-copy-id
+  tmux
+  unar
+  vim
+  weechat
+  wget
+  wifi-password
+)
+
+coding=(
+  clang-format
+  cscope
+  gcc
+  gdb
+  jq
+  hugo
+  llvm
+  lua
+  mysql
+  perl
+  postgresql
+  protobuf
+  python
+  python3
+  rbenv
+  ruby
+)
+
+media=(
+  faad2
+  ffmpeg
+  libav
+  youtube-dl
+)
+
+# install apps and whatnots
+brew install "${apps[@]}"
+brew install "${coding[@]}"
+brew install "${media[@]}"
+
+# remove outdated versions from the cellar
+brew cleanup
+
+# start chunkwm and khd by default
+brew services start koekeishiya/formulae/skhd
+brew services restart crisidev/chunkwm/chunkwm
